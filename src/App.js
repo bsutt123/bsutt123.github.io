95aa14d375bf84be2281154f72f3b0c22f31235c
Brady Sutton <bsutt123@gmail.com>
2018-03-07 10:01:23 -0500

    implement async component in App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react';
 import {BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 
-
-
-import Hero from  './components/Hero';
-import AboutMe from './components/AboutMe';
-import SkillsList from './components/SkillsList';
+import asyncComponent from './components/AsyncComponent';
 import FixedNav from './components/FixedNav';
-import Projects from './components/Projects';
-import Blog from './components/Blog';
+
+const AsyncHero = asyncComponent(() => import('./components/Hero'));
+const AsyncAboutMe =  asyncComponent(() => import('./components/AboutMe'));
+const AsyncSkillsList = asyncComponent(() => import('./components/SkillsList'));
+const AsyncProjects =  asyncComponent(() => import('./components/Projects'));
+const AsyncBlog = asyncComponent(() => import('./components/Blog'))
+
+
 
 class App extends Component {
   render() {
@@ -18,11 +20,11 @@ class App extends Component {
         <Router>
           <div>
             <FixedNav />
-            <Route exact path="/" component={Hero} />
-            <Route path="/about-me" component={AboutMe} />
-            <Route path="/skills" component={SkillsList} />
-            <Route path="/projects" component={Projects} />
-            <Route path="/blog" component={Blog} />
+            <Route exact path="/" component={AsyncHero} />
+            <Route path="/about-me" component={AsyncAboutMe} />
+            <Route path="/skills" component={AsyncSkillsList} />
+            <Route path="/projects" component={AsyncProjects} />
+            <Route path="/blog" component={AsyncBlog} />
           </div>
         </Router>
       </div>
