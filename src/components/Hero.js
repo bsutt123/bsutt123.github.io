2df546c4f72084227ac5cb24225878e6e308807d
Brady <bsutt123@gmail.com>
2018-04-01 08:50:29 -0400

    Update Hero with Prettier

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,26 +1,26 @@
-import React, {Component} from 'react';
-import styles from 'Styles/modules/hero.module.css'
+import React, { Component } from "react";
+import styles from "Styles/modules/hero.module.css";
 
-
-import engineer from '../img/mech_engineer.jpg';
-import webDev from '../img/dev-typing.jpg';
+import engineer from "../img/mech_engineer.jpg";
+import webDev from "../img/dev-typing.jpg";
 
 class Hero extends Component {
-    render() {
-        return (
-            <div className={styles.hero}>
-                <img className={styles.heroImgTop} src={engineer} alt="engineering" />
-                <img className={`${styles.heroImg}`} src={webDev} alt="computer" />
-                <div className={styles.heroTitle}>
-                    <h1 className={styles.headingPrimary} >Brady Sutton</h1>
-                    <h3 className={styles.headingSecondary} >
-                        <span className={styles.fadeIn}> Former Mechanical engineer</span> ... 
-                        <span className={styles.fadeOut}> turned software developer </span>
-                    </h3>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className={styles.hero}>
+        <img className={styles.heroImgTop} src={engineer} alt="engineering" />
+        <img className={`${styles.heroImg}`} src={webDev} alt="computer" />
+        <div className={styles.heroTitle}>
+          <h1 className={styles.headingPrimary}>Brady Sutton</h1>
+          <h3 className={styles.headingSecondary}>
+            <span className={styles.fadeIn}> Former Mechanical engineer</span>
+            ...
+            <span className={styles.fadeOut}> turned software developer </span>
+          </h3>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Hero;
